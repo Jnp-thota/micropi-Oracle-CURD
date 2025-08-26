9c3cf436a945db6707da6cd18ba691b0aff9aade
Jaya Naga Prakash Thota <jnprakashthota@gmail.com>
2025-08-25 17:12:52 -0700

    update the code for testing

--- src/main/java/com/jnpThota/oracleDB/controller/workerController.java
+++ src/main/java/com/jnpThota/oracleDB/controller/workerController.java
@@ -3,25 +3,46 @@ package com.jnpThota.oracleDB.controller;
 
 import java.util.List;
 
+import jakarta.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.jnpThota.oracleDB.model.Employees;
 import com.jnpThota.oracleDB.service.EmployeeService;
 
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/v1/api")
 public class workerController {
 	@Autowired
 	private EmployeeService service;
 
-	@GetMapping("/getEmployees")
-	public List<Employees> viewHomePage()
+	@GetMapping("/liteworker")
+	public List<Employees> liteWorker()
 	{
-		return service.listAll();
+		return service.employeesList();
+	}
+
+	@GetMapping("/worker/{emplID}")
+	public Employees worker(@PathVariable("emplID") Long emplID){
+		return service.workerData(emplID);
+
+	}
+
+	@PostMapping("/worker")
+	public Employees addEmployee(@RequestBody Employees employee) {
+		if(service.saveEmployee(employee)){
+			return employee;
+		}
+		return null;
+    }
+
+	@DeleteMapping("/worker/{emplID}")
+	public String deleteEmployee(@PathVariable("emplID") Long emplID) {
+		if(service.removeEmployee(emplID)){
+			return "Employee Sucessfully deleted";
+		}
+		return "Employee deletion Failed";
 	}
 	
 
--- src/main/java/com/jnpThota/oracleDB/service/EmployeeService.java
+++ src/main/java/com/jnpThota/oracleDB/service/EmployeeService.java
@@ -1,5 +1,6 @@
 package com.jnpThota.oracleDB.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -7,28 +8,49 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.jnpThota.oracleDB.model.Employees;
-import com.jnpThota.oracleDB.repository.EmployeeRepository;
+//import com.jnpThota.oracleDB.repository.EmployeeRepository;
 
 @Service
 public class EmployeeService {
     
-    @Autowired
-    private EmployeeRepository repo;
-    
-    public List<Employees> listAll() {
-        return repo.findAll();
+//    @Autowired
+//    private EmployeeRepository repo;
+
+    List<Employees> employeesDB = new ArrayList<>();
+    public List<Employees> employeesList() {
+          return employeesDB;
+//        return repo.findAll();
     }
     
-    public void save(Employees employee) {
-        repo.save(employee);
+    public boolean saveEmployee(Employees employee) {
+        try {
+            employeesDB.add(employee);
+        }catch (Exception e){
+            return false;
+        }
+        return true;
+    }
+
+    public Employees workerData(Long id) {
+
+        for(Employees employee : employeesDB){
+            if(employee.getEmpId().equals(id)){
+                return employee;
+            }
+        }
+        return null;
+//        Optional<Employees> result = repo.findById(id);
+//        return result.orElse(null);
     }
     
-    public Employees get(Long id) {
-        Optional<Employees> result = repo.findById(id);
-        return result.orElse(null);
-    }
-    
-    public void delete(Long id) {
-        repo.deleteById(id);
+    public boolean removeEmployee(Long id) {
+        for(Employees employee : employeesDB){
+            if(employee.getEmpId().equals(id)){
+                employeesDB.remove(employee);
+                return true;
+            }
+        }
+        return false;
+//        repo.deleteById(id);
     }
 }
